d68c4ba818bfd017f13389e125ba5aaaf3cd3ad0
Fernando Bach <fernandobach02@gmail.com>
2026-09-20 22:24:42 -0300

    Organiza estrutura dos scripts SQL

---
+++ SQL/02_rollback.sql
@@ -0,0 +1,16 @@
+USE CarteiraVirtual;
+GO
+
+-- ============================================
+-- 02_ROLLBACK.SQL
+--
+-- Objetivo:
+-- Remover todos os objetos criados pelo
+-- 01_criacao.sql na ordem correta.
+--
+-- Ordem de remoção:
+-- 1. Índices
+-- 2. Tabelas
+-- 3. Schema
+-- 4. Banco de dados
+-- ============================================
---
+++ SQL/03_insert.sql
@@ -0,0 +1,16 @@
+USE CarteiraVirtual;
+GO
+
+
+-- ============================================
+-- 03_INSERT.SQL
+--
+-- Objetivo:
+-- Inserir dados de exemplo nas tabelas:
+-- • Moeda
+-- • Corretora
+-- • Cliente
+-- • Carteira
+-- • ItemCarteira
+-- • ParesMoedas
+-- ============================================
